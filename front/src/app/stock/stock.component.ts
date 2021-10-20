1752bb3c7de85c9247f5ba08e70ea4d57fd9139d
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-20 17:02:50 +0200

    dev: sonarlint

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,20 +1,16 @@
+import { Component } from '@angular/core';
 import { Article } from './../interfaces/article';
-import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss'],
 })
-export class StockComponent implements OnInit {
+export class StockComponent {
   articles: Article[] = [
     { name: 'Tournevis', price: 1.23, qty: 234 },
     { name: 'Pelle', price: 2.4, qty: 120 },
     { name: 'Marteau', price: 3, qty: 5 },
     { name: 'Pince', price: 5, qty: 1200 },
   ];
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
