de2ab169d3b69390c16a3545e05c283bfc9c731c
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-20 17:01:52 +0200

    dev: component stock contains article array

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,20 @@
+import { Article } from './../interfaces/article';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Article[] = [
+    { name: 'Tournevis', price: 1.23, qty: 234 },
+    { name: 'Pelle', price: 2.4, qty: 120 },
+    { name: 'Marteau', price: 3, qty: 5 },
+    { name: 'Pince', price: 5, qty: 1200 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
