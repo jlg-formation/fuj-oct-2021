d689b5ff517c2f011413384f53631a836e9e17ba
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-20 17:19:41 +0200

    dev: added pipe currency

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 
+registerLocaleData(localeFr, 'fr');
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LegalComponent
+  declarations: [AppComponent, HomeComponent, LegalComponent],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    LayoutModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -11,6 +11,6 @@ export class StockComponent {
     { name: 'Tournevis', price: 1.23, qty: 234 },
     { name: 'Pelle', price: 2.4, qty: 120 },
     { name: 'Marteau', price: 3, qty: 5 },
-    { name: 'Pince', price: 5, qty: 1200 },
+    { name: 'Pince', price: 235, qty: 1200 },
   ];
 }
