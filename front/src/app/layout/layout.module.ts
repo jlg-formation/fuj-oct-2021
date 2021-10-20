aece5e48545cee158b8f30659fbb7006c9cb8d86
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-10-20 12:12:37 +0200

    dev: header ok

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -4,16 +4,9 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 
-
-
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    BodyComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, FooterComponent, BodyComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent, BodyComponent],
 })
-export class LayoutModule { }
+export class LayoutModule {}
